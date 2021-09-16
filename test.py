4849fb2f8a5c42334384af050fe3e9879b23a1ce
Nick <nicolas.cinera@gmail.com>
2021-09-15 23:45:57 -0400

    Unimportant

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-string = "bruh moment"
-subs = "bruh"
+n = 1001
+def sumT(n):
+    if n <= 1:
+        return n
+    else:
+        return n + sumT(n-1)
+
+def oddSum(n):
+    return n * n
+
+print(sumT(n), oddSum(n))
