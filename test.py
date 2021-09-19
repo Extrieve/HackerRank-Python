852ff6cf09c910d85fd2a0b6ef85322eb063170a
Nick <nicolas.cinera@gmail.com>
2021-09-19 14:20:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-n = 1001
-def sumT(n):
-    if n <= 1:
-        return n
-    else:
-        return n + sumT(n-1)
+def minSubArrayLen(s, nums):
+    total = left = 0
+    result = len(nums) + 1
+    for right, n in enumerate(nums):
+        total += n
+        print(total)
+        while total >= s:
+            #print(result)
+            result = min(result, right - left + 1)
+            total -= nums[left]
+            left += 1
+            #print(result, left)
+    return result if result <= len(nums) else 0
 
-def oddSum(n):
-    return n * n
-
-print(sumT(n), oddSum(n))
+if __name__ == '__main__':
+    print(minSubArrayLen(7, [2,3,1,2,4,3]))
