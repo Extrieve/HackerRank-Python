a982d26ff2b7d89ef34aef3beb7a974a78443287
Nick <nicolas.cinera@gmail.com>
2021-09-24 22:15:27 -0400

    Update concat_swaps.py

--- concat_swaps.py
+++ concat_swaps.py
@@ -1,11 +1,16 @@
 def concatSwaps(s, sizes):
-    concatList = [] * len(sizes)
-    for i in range(0, len(sizes)):
-        if i == 0:
-            concatList.append(s[:sizes[i] + 1])
-        else:
-            concatList.append(s[sizes[i-1]:sizes[i-1] + sizes[i] + 1])
+    concatList = [''] * len(sizes)
+    currentPos = 0
+    for i in range(0, len(sizes), 2):
+        try:
+            print(i, i+1)
+            concatList[i] = s[currentPos:sizes[i]+1]
+            currentPos += sizes[i]
+            concatList[i + 1] = s[currentPos:currentPos + sizes[i+1]]
 
+            concatList[i], concatList[i+1] = concatList[i+1], concatList[i]
+        except IndexError:
+            concatList[-1] = s[currentPos+sizes[-2]:]
     return ''.join(concatList)
 
 
