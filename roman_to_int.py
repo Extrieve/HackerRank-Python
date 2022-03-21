1c48e4090f7c2f9f09d82f3019782fdc98cd1520
Nick <nicolas.cinera@gmail.com>
2022-03-20 23:43:59 -0400

    Leetcode XD
    - XD

---
+++ roman_to_int.py
@@ -0,0 +1,22 @@
+def romanToInt(s):
+    romans = {'I': 1, 'V': 5, 'IV': 4, 'X': 10, 'IX': 9, 'L': 50, 'XL': 40,
+              'C': 100, 'XC': 90, 'D': 500, 'CD': 400, 'M': 1000, 'CM': 900}
+
+    i = 0
+    num = 0
+    while i < len(s):
+        try:
+            if s[i] + s[i+1] not in romans:
+                num += romans[s[i]]
+                i += 1
+            else:
+                num += romans[s[i] + s[i+1]]
+                i += 2
+        except:
+            num += romans[s[-1]]
+            break
+    
+    return num
+
+if __name__ == '__main__':
+    print(romanToInt('LVIII'))
