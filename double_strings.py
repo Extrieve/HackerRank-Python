f67da369cde28e15fe8720bca7c9ec7a1dcb7bee
Nick <nicolas.cinera@gmail.com>
2022-08-16 21:34:43 -0500

    Codeforces D String Warmup

---
+++ double_strings.py
@@ -0,0 +1,46 @@
+def d_strings(s):
+    output = []
+    for i, cur in enumerate(s):
+        flag = False
+        for j, t_string in enumerate(s):
+            for k, k_string in enumerate(s):
+                if cur == (t_string + k_string):
+                    flag = True
+                    break
+
+        output.append('0') if not flag else output.append('1')
+
+    return ''.join(output)
+
+def d_string1(strings, length):
+    mymap = {}
+    for i in range(length):
+        for j in range(length):
+            if strings[i] + strings[j] not in mymap:
+                mymap[strings[i] + strings[j]] = True
+            # elif strings[j] + strings[i] not in mymap:
+            #     mymap[strings[j] + strings[i]] = True
+            else:
+                continue
+
+    output = []
+    for string in strings:
+        flag = False
+        if string in mymap:
+            flag = True
+        output.append('0') if not flag else output.append('1')
+
+    return ''.join(output)
+
+
+if __name__ == '__main__':
+    x = int(input())
+    for _ in range(x):
+        
+        n = int(input())
+        myinput = []
+
+        for _ in range(n):
+            myinput.append(input())
+
+        print(d_string1(myinput, n))
