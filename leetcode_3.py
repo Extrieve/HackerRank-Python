fcf125e5287790f187b37c88c521177ff8959cec
Nick <nicolas.cinera@gmail.com>
2022-08-18 18:08:50 -0500

    Create leetcode_3.py

---
+++ leetcode_3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, string: str) -> int:
+        ss = ""
+        maxLength = 0
+        for c in string:
+            lastIndex = ss.find(c)
+            if lastIndex > -1:
+                ss = ss[lastIndex + 1:]
+            else:
+                return len(string[lastIndex + 1:])
+            ss += c
+            maxLength = max(maxLength, len(ss))
+        return maxLength
