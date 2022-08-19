d923b01250604edd75087e9870a49ef3899b673b
Nick <nicolas.cinera@gmail.com>
2022-08-19 10:05:14 -0500

    Better

--- leetcode_121.py
+++ leetcode_121.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxProfit(self, arr: list[int]) -> int:
-
+        
         ans, left = 0, 0
 
         for r in range(1, len(arr)):
             if arr[r] < arr[left]:
                 left = r
-
-            ans = max(ans, arr[r] - arr[left])
+            else:
+                ans = max(ans, arr[r] - arr[left])
 
         return ans
