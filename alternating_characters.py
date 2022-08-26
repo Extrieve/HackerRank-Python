45ffb494f8b9a49d3fe9d040a344539462bfda2a
Nick <nicolas.cinera@gmail.com>
2022-08-25 20:38:15 -0400

    Create alternating_characters.py

---
+++ alternating_characters.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+# https://www.hackerrank.com/challenges/alternating-characters/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=strings
+def alternatingCharacters(string):
+    # Write your code here
+    st = string[0]
+    for s in string[1:]:
+        if st[-1] != s:
+            st += s
+    return len(string) - len(st)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
